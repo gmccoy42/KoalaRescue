9aa69adaa701d7f65a51e916e269e8936035ca1a
unknown <gmccoy4242@gmail.com>
2014-11-26 14:25:47 -0500

    First commit

---
+++ Assets/follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour 
+{
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3(player.transform.position.x, -0.1f, -10);
+	}
+}
---
+++ Assets/invisible.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnBecameInvisible () 
+{
+	transform.Translate(renderer.bounds.size.x * 3, 0, 0);
+    print("Invisible");
+}
